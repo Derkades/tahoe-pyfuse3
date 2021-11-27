3af3e6f46b983649c166396b9ea1a92d8a11bb73
Robin <robinslot@hotmail.nl>
2021-11-27 16:20:43 +0100

    Remove reference to tahoe-upload in build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -ex
-docker build -t derkades/tahoe-mount -f Dockerfile.mount .
-docker build -t derkades/tahoe-upload -f Dockerfile.upload .
+docker build -t derkades/tahoe-mount -f Dockerfile .
