060c00ccfced5315c8761d1560abab44e9294124
Robin <robinslot@hotmail.nl>
2022-01-09 20:43:12 +0100

    more proper docker build/push script

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-set -ex
-docker build -t derkades/tahoe-mount -f Dockerfile .
+set -e
+
+IMAGE="derkades/tahoe-mount"
+MAJOR="1"
+MINOR="2"
+PATCH="0"
+
+docker build \
+    -t "$IMAGE" \
+    -t "$IMAGE:$MAJOR" \
+    -t "$IMAGE:$MAJOR.$MINOR" \
+    -t "$IMAGE:$MAJOR.$MINOR.$PATCH" \
+    -f Dockerfile .
+
+if [ "$1" == "push" ]
+then
+    docker push "$IMAGE"
+    docker push "$IMAGE:$MAJOR"
+    docker push "$IMAGE:$MAJOR.$MINOR"
+    docker push "$IMAGE:$MAJOR.$MINOR.$PATCH"
+fi
